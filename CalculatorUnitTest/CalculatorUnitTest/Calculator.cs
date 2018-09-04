7172e1c9ef6ecd027ea8df11724dd88a5ea50899
Anne Porsgaard <anne@besenbacher.com>
2018-09-04 09:41:48 +0200

    Added an accumulator to all parts

--- CalculatorUnitTest/CalculatorUnitTest/Calculator.cs
+++ CalculatorUnitTest/CalculatorUnitTest/Calculator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,24 +9,34 @@ namespace Calculator
 {
     public class Calculator
     {
+        private double _accumulator = 0;
+
         public double Add(double a, double b)
         {
-            return a + b; 
+            double result = a + b;
+            Accumulator = result;
+            return result;
         }
 
         public double Subtract(double a, double b)
         {
-            return a - b; 
+            double result = a - b;
+            Accumulator = result;
+            return result;
         }
 
         public double Multiply(double a, double b)
         {
-            return a * b;
+            double result = a * b;
+            Accumulator = result;
+            return result;
         }
 
         public double Power(double x, double exp)
         {
-            return Math.Pow(x, exp);
+            double result = Math.Pow(x, exp);
+            Accumulator = result;
+            return result;
         }
 
         public double Divide(double dividend, double divisor)
@@ -35,7 +46,48 @@ namespace Calculator
                 return Double.NaN;
             }
 
-            return dividend / divisor;
+            double result = dividend / divisor;
+            Accumulator = result;
+            return result;
+        }
+
+        public double Accumulator
+        {
+            get { return _accumulator; }
+            private set
+            {
+                _accumulator = value;
+            }
+        }
+
+        public void Clear()
+        {
+            Accumulator = 0;
+        }
+
+        public double Add(double addend)
+        {
+            return Add(Accumulator, addend);
+        }
+
+        public double Substract(double subtractor)
+        {
+            return Subtract(Accumulator, subtractor);
+        }
+
+        public double Multiply(double multiplier)
+        {
+            return Multiply(Accumulator, multiplier);
+        }
+
+        public double Divide(double divisor)
+        {
+            return Divide(Accumulator, divisor);
+        }
+
+        public double Power(double exponent)
+        {
+            return Power(Accumulator, exponent);
         }
     }
 }
