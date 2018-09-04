ff3d4fa712110fc2e021f2fe6ca91064489a80a9
Anne Porsgaard <anne@besenbacher.com>
2018-09-04 09:56:44 +0200

    Calculator solution with hand testing

---
+++ Calculator/Calculator_test/CalculatorTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Application
+{
+    class CalculatorTest
+    {
+        static void Main(string[] args)
+        {
+            //Test addition
+            Calculator calculation = new Calculator();
+            Console.WriteLine(calculation.Add(2, 2));
+            Console.WriteLine(calculation.Add(-4, -2));
+            //Test substraction
+            Console.WriteLine(calculation.Subtract(5,3));
+            Console.WriteLine(calculation.Subtract(-5,-2));
+            //Test Multiplication
+            Console.WriteLine(calculation.Multiply(3,2));
+            Console.WriteLine(calculation.Multiply(-2, -4));
+            Console.WriteLine(calculation.Multiply(3, 0));
+            //Test power
+            Console.WriteLine(calculation.Power(2,4));
+            Console.WriteLine(calculation.Power(2, -4));
+            Console.WriteLine(calculation.Power(-2, 4));
+            Console.WriteLine(calculation.Power(-2, -4));
+
+
+        }
+    }
+}
