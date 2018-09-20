8a23a5fd345ad1e758940b9464a729c3af6c0455
Anne Porsgaard <anne@besenbacher.com>
2018-09-20 13:02:55 +0200

    Made coverage test

---
+++ Fakes2/Fakes2/DoorControl.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fakes2
+{
+    public class DoorControl
+    {
+        private readonly IUserValidation _uv;
+        private readonly IEntryNotification _en;
+        private readonly IDoor _door;
+        private readonly IAlarm _alarm;
+
+        private bool CurrentDoorState = false;
+
+        public DoorControl(IUserValidation uv, IEntryNotification en, IDoor door, IAlarm alarm)
+        {
+            _uv = uv;
+            _en = en;
+            _door = door;
+            _alarm = alarm;
+        }
+        public void RequestEntry(string id)
+        {
+            if (_uv.ValidateEntryRequest(id) == true)
+            {
+                _door.Open();
+                _en.NotifyEntryGranted();
+                CurrentDoorState = true;
+            }
+            else if (_uv.ValidateEntryRequest(id) == false)
+            {
+                _en.NotifyEntryDenied();
+            }
+        }
+
+        public void DoorOpened()
+        {
+            if (CurrentDoorState == false) 
+            {
+                _door.Closed();
+                _alarm.RaiseAlarm();
+            }
+            else if (CurrentDoorState == true)
+            {
+               _door.Closed();
+                CurrentDoorState = false;
+            }
+        }
+
+        public void DoorClose()
+        {
+
+        }
+    }
+}
---
+++ Fakes2/Fakes2_Unit_Test/DoorUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Fakes2;
+using NUnit.Framework;
+using NSubstitute;
+
+namespace Fakes2_Unit_Test
+{
+    [TestFixture]
+    public class DoorUnitTest
+    {
+        private DoorControl _uut;
+        private IUserValidation _uv;
+        private IEntryNotification _en;
+        private IDoor _door;
+        private IAlarm _alarm;
+
+        [SetUp]
+
+        public void SetUp()
+        {
+            _uv = Substitute.For<IUserValidation>();
+            _en = Substitute.For<IEntryNotification>();
+            _door = Substitute.For<IDoor>();
+            _alarm = Substitute.For<IAlarm>();
+
+            _uut = new DoorControl(_uv, _en, _door, _alarm);
+        }
+
+        [Test]
+
+        public void 
+    }
+}
